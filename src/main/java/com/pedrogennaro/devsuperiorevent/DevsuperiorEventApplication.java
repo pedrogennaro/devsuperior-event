4532b903adad8090646d81f78899f883f42f0fe9
Pedro De Gennaro <pedrogennaro@gmail.com>
2024-05-20 09:33:02 -0300

    first commit

---
+++ src/main/java/com/pedrogennaro/devsuperiorevent/DevsuperiorEventApplication.java
@@ -0,0 +1,13 @@
+package com.pedrogennaro.devsuperiorevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsuperiorEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsuperiorEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrogennaro/devsuperiorevent/DevsuperiorEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrogennaro.devsuperiorevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
